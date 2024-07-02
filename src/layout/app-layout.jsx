9bae1f3f1584a78ec792123739501e465ebfe583
novanishant <nishantverma3633@gmail.com>
2024-07-02 09:04:14 +0530

    header added

--- src/layout/app-layout.jsx
+++ src/layout/app-layout.jsx
@@ -1,10 +1,12 @@
 // import Header from "@/components/header";
+import Header from "@/components/header";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
       <main className="min-h-screen container">
+        <Header />
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
--- src/pages/link.jsx.jsx
+++ src/pages/link.jsx.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 const LinkPage = () => {
   return <div>LinkPage</div>;
