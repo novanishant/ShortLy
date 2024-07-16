86d21f87ae745eca6a3f81688877c25420390a69
novanishant <nishantverma3633@gmail.com>
2024-07-16 22:52:39 +0530

    short-ly-lac.vercel.app navigation added

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://shortly.in/${link}`}
+            href={`https://short-ly-lac.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://shortly.in/{link}
+            https://short-ly-lac.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://shortly.in/${link}`)
+                navigator.clipboard.writeText(`https://short-ly-lac.vercel.app/${link}`)
               }
             >
               <Copy />
